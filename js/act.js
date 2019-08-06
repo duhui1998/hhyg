8b6c57c99371c56f77adac64a4352ceb8a254ea0
duhui1998 <799408285@qq.com>
2019-08-06 12:45:01 +0800

    提交

---
+++ js/act.js
@@ -0,0 +1,8 @@
+       $(document).ready(function (){// ready 事件
+         	$('nav li a').click(function() {
+         		   $('nav li a').removeClass();
+            $(this).addClass("act");
+
+         	});
+             
+                      });
